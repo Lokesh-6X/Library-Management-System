f78ae6d191a194226fa69b2ebbacb940d2463f60
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-10 00:19:24 +0530

    Written logic for the api's

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -3,37 +3,64 @@ const router = express.Router();
 const Book = require("../models/bookModel");
 
 //Create a book
-router.route("/").post((req, res) => {
-    res.status(200).send("Added a book");
+router.route("/").post(async (req, res) => {
+  const { title, author } = req.body;
+  if (!title || !author) {
+    res.status(400);
+  }
+  const book = await Book.create({
+    title,
+    author,
+  });
+  res.status(200).json(book);
 });
 
 //Fetch book by id
-router.route("/id/:id").get((req, res) => {
-    res.status(200).send("fetched a book");
+router.route("/id/:id").get(async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  if (!book) {
+    res.status(404);
+  }
+  res.status(200).json(book);
 });
 
 //Search book by name
 //Lets use query parameter here
-router.route("/name/:name").get((req, res) => {
-    res.status(200).send("fetched a book by name");
+router.route("/title/:title").get(async (req, res) => {
+  const book = await Book.findOne({ title: req.params.title });
+  if (!book) {
+    res.status(404);
+  }
+  res.status(200).json(book);
 });
 
 //Fetch all books
-router.route("/").get( async (req, res) => {
-    const book = await Book.find();
-    res.status(200).json(book);
+router.route("/").get(async (req, res) => {
+  const book = await Book.find();
+  res.status(200).json(book);
 });
 
 //Update a book
-router.route("/:id").put((req, res) => {
-    res.status(200).send("Updated a book");
+router.route("/:id").put(async (req, res) => {
+  const book = await Book.findById(req.params.id);
+  const { title, author } = req.body;
+  if (!book) {
+    res.status(404);
+  }
+  const updateBook = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateBook);
 });
 
 //Delete a book
-router.route("/:id").delete((req, res) => {
-    res.status(200).send("Deleted a book");
+router.route("/:id").delete(async (req, res) => {
+  const book = Book.findById(req.params.id);
+  if (!book) {
+    res.status(404);
+  }
+  const deleteBook = await Book.findByIdAndDelete(req.params.id);
+  res.status(200).json(deleteBook);
 });
-
-
 
 module.exports = router;
