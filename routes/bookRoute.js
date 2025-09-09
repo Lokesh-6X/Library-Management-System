316852f8fd9dad20ab03fb73cbfbda7ed02ab439
Lokesh-6X <s.lokeshkumar068@gmail.com>
2025-09-09 22:28:13 +0530

    Edited API endpoints

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -7,14 +7,14 @@ router.route("/").post((req, res) => {
 });
 
 //Fetch book by id
-router.route("/:id").get((req, res) => {
+router.route("id/:id").get((req, res) => {
     res.status(200).send("fetched a book");
 });
 
-//Search book bu name
+//Search book by name
 //Lets use query parameter here
-router.route("/").get((req, res) => {
-    res.status(200).send("fetched a book by author");
+router.route("name/:name").get((req, res) => {
+    res.status(200).send("fetched a book by name");
 });
 
 //Fetch all books
@@ -23,12 +23,12 @@ router.route("/").get((req, res) => {
 });
 
 //Update a book
-router.route("/").put((req, res) => {
+router.route("/:id").put((req, res) => {
     res.status(200).send("Updated a book");
 });
 
 //Delete a book
-router.route("/").delete((req, res) => {
+router.route("/:id").delete((req, res) => {
     res.status(200).send("Deleted a book");
 });
 
